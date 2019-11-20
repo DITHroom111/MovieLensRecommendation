6dc94bcb87f57a8b2d8d67a2b966e93966c86528
Pavel Koshkin <koshkin.pasha@gmail.com>
2019-11-20 16:30:40 +0300

    many namespaces

--- src/linear_models/make_vw_cv.py
+++ src/linear_models/make_vw_cv.py
@@ -4,9 +4,30 @@ import os
 import shutil
 
 
+def make_namespace(joined_words, name):
+    return "{}| LENGTH:{} {}".format(name, len(joined_words.split()), words)
+
+
 def make_vw_line(line):
-    fold, user_id, movie_id, timestamp, rating, words = line[:-1].split("\t")
-    return "{} |height_rating_intersection {}".format(rating, words)
+    split = line[:-1].split("\t")
+
+    assert len(split) == 12
+    fold, user_id, movie_id, timestamp = map(int, split[:4])
+    rating = float(split[5])
+    height_rating_intersetion_words, low_rating_intersetion_words = split[5:7]
+    height_rating_intersetion_title_words, low_rating_intersetion_title_words = split[7:9]
+    movie_title, user_height_rating_titles, user_low_rating_titles = split[9:12]
+
+    vw_line = "{}".format(rating)
+    vw_line += make_namespace(height_rating_intersetion_words, "height_rating_intersetion_words")
+    vw_line += make_namespace(low_rating_intersetion_words , "low_rating_intersetion_words ")
+    vw_line += make_namespace(height_rating_intersetion_title_words, "height_rating_intersetion_title_words")
+    vw_line += make_namespace(low_rating_intersetion_title_words, "low_rating_intersetion_title_words")
+    vw_line += make_namespace(movie_title, "movie_title")
+    vw_line += make_namespace(user_height_rating_titles, "user_height_rating_titles")
+    vw_line += make_namespace(user_low_rating_titles, "user_low_rating_titles")
+
+    return vw_line
 
 
 def create_fodler(folder):
